556feccbe6d56e12c8972ef909863317c8406a32
ericakathlen <kathlen.erica@gmail.com>
2024-06-16 09:27:53 -0300

    Tentativa 1

--- Backend com Java - Santander 2024/SIMULACAO CONTABANCARIA/CONTABANCO/src/ContaTerminal.java
+++ Backend com Java - Santander 2024/SIMULACAO CONTABANCARIA/CONTABANCO/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Importar a classe Scanner
+        Scanner scanner = new Scanner(System.in);
+        // Solicita o numero da agencia
+        System.out.println("Por favor, digite o numero da agencia: ");
+        String agencia = scanner.nextLine();
+        // Solicita o numero da conta
+        System.out.print("Por favor, digite o número da conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Consumir a nova linha
+        //Solicita o nome do cliente
+        System.out.println("Por favor, digite o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+        // Solicita e recebe o saldo
+        System.out.print("Por favor, digite o saldo: ");
+        double saldo = scanner.nextDouble();
+        
+        // Fecha o scanner
+        scanner.close();
+
+        // Exibir as msgs para o usuario
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
